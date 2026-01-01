2dd66b0e20250e1f1b2413f13e5595750d35e63b
Comrade X <106185020+comrade09@users.noreply.github.com>
2026-01-02 01:42:39 +0530

    Implement video retrieval and deletion functions
    Added functions to get and delete videos by code.

--- database/videos.py
+++ database/videos.py
@@ -5,9 +5,16 @@ client = pymongo.MongoClient(DB_URI)
 db = client[DB_NAME]
 
 videos = db["videos"]
+
+# Make sure each code is unique
 videos.create_index("code", unique=True)
 
 def add_video(code, file_id, message_id):
+    """
+    Add a new video.
+    Returns True if added.
+    Returns False if code already exists.
+    """
     try:
         videos.insert_one({
             "code": code,
@@ -18,5 +25,27 @@ def add_video(code, file_id, message_id):
     except pymongo.errors.DuplicateKeyError:
         return False
 
+def get_video(code):
+    """
+    Get video by code.
+    Returns document or None.
+    """
+    return videos.find_one({"code": code})
+
+def delete_video(code):
+    """
+    Delete a video by code.
+    """
+    return videos.delete_one({"code": code})
+
 def count():
+    """
+    Total indexed videos.
+    """
     return videos.count_documents({})
+
+def all_codes():
+    """
+    Return list of all codes.
+    """
+    return [x["code"] for x in videos.find({}, {"code": 1})]
