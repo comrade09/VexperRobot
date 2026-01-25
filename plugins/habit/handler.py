ec06fd9a088c1d1398808d2d2008e014ae68c298
Comrade X <106185020+comrade09@users.noreply.github.com>
2026-01-25 22:51:03 +0530

    Update handler.py

--- plugins/habit/handler.py
+++ plugins/habit/handler.py
@@ -27,7 +27,7 @@ habits_col = database["habits"]
 
 # ================= ADD HABIT =================
 
-@bot.on_message(filters.command("addhabit"),group=56756)
+@Bot.on_message(filters.command("addhabit"),group=56756)
 async def add_habit(_, msg):
 
     if len(msg.command) < 2:
@@ -52,7 +52,7 @@ async def add_habit(_, msg):
 
 # ================= /LOG =================
 
-@bot.on_message(filters.command("log"),group=5675976)
+@Bot.on_message(filters.command("log"),group=5675976)
 async def log_menu(_, msg):
 
     uid = msg.from_user.id
@@ -77,7 +77,7 @@ async def log_menu(_, msg):
 
 # ================= SELECT HABIT =================
 
-@bot.on_callback_query(filters.regex("^log:"),group=56745256)
+@Bot.on_callback_query(filters.regex("^log:"),group=56745256)
 async def select_habit(_, cq):
 
     hid = cq.data.split(":")[1]
@@ -127,7 +127,7 @@ async def select_habit(_, cq):
 
 # ================= MARK DAY =================
 
-@bot.on_callback_query(filters.regex("^mark:"),group=5675536)
+@Bot.on_callback_query(filters.regex("^mark:"),group=5675536)
 async def mark_day(_, cq):
 
     _, hid, day = cq.data.split(":")
@@ -163,7 +163,7 @@ async def mark_day(_, cq):
 
 # ================= WEEKLY =================
 
-@bot.on_message(filters.command("weekly"),group=5676656)
+@Bot.on_message(filters.command("weekly"),group=5676656)
 async def weekly_report(_, msg):
 
     uid = msg.from_user.id
@@ -202,7 +202,7 @@ async def weekly_report(_, msg):
 
 # ================= HEATMAP =================
 
-@bot.on_message(filters.command("heatmap"),group=561298756)
+@Bot.on_message(filters.command("heatmap"),group=561298756)
 async def heatmap(_, msg):
 
     uid = msg.from_user.id
@@ -244,7 +244,7 @@ async def heatmap(_, msg):
 
 # ================= MONTH =================
 
-@bot.on_message(filters.command("month"),group=56436756)
+@Bot.on_message(filters.command("month"),group=56436756)
 async def month(_, msg):
 
     uid = msg.from_user.id
@@ -288,7 +288,7 @@ async def month(_, msg):
 
 # ================= SINGLE HABIT =================
 
-@bot.on_message(filters.command("habit"),group=567566767)
+@Bot.on_message(filters.command("habit"),group=567566767)
 async def habit_stats(_, msg):
 
     if len(msg.command) < 2:
